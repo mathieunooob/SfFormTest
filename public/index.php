418b6cd0c53b16030381acf1016a12af272c4e7b
mathieunoob <mat.webdev.1982@gmail.com>
2019-10-23 11:57:21 +0200

    mise au propre du .env

--- public/index.php
+++ public/index.php
@@ -20,7 +20,12 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
     Request::setTrustedHosts([$trustedHosts]);
 }
 
+// Ligne pour le dev
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+
+// Ligne pour la prod
+// $kernel = new Kernel($_SERVER['APP_ENV'], false);
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
